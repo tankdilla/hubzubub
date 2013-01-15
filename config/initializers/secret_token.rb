d3a17f6fda21dbe7cfde312ede7ec045ec6e0620
Darrell Johnson <darrellj@gmail.com>
2013-01-15 11:48:01 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubzubub::Application.config.secret_token = '1ca384a9e20b8d23125587a72135c9255117ebd6a57781d41efe887d626cc4bc5ff62ad3495342d8eef41c2a5f995c961125731094986085ff90a860c4ee9dad'
